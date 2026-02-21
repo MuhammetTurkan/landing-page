11b8e105eb978bd5a0dcbf689f05311361888283
MuhammetTurkan <muhammet.trkn98@gmail.com>
2026-02-21 15:00:49 +0300

    create: create the brands sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Layout } from "./components/Layout";
+import Brands from "./components/sections/Brands";
 import Hero from "./components/sections/Hero";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <>
       <Layout title="Landing Page">
         <Hero />
+        <Brands />
       </Layout>
     </>
   );
