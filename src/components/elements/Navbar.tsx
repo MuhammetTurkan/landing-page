5e447c221c237b4489bacb01f719ac164c7e8c68
MuhammetTurkan <muhammet.trkn98@gmail.com>
2026-03-08 14:36:04 +0300

    create: create the pricing section

--- src/components/elements/Navbar.tsx
+++ src/components/elements/Navbar.tsx
@@ -9,7 +9,7 @@ const navItems = [
   { href: "#", text: "Home" },
   { href: "#services", text: "Services" },
   { href: "#about-us", text: "About Us" },
-  { href: "#features", text: "Features" },
+  { href: "#pricing", text: "Pricing" },
 ];
 
 const Navbar = () => {
